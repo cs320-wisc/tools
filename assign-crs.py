7a29866708460591a73bda0317286afbeab8b2a3
Tyler <tharter@wisc.edu>
2021-09-29 20:13:13 +0000

    CR assignment script

---
+++ assign-crs.py
@@ -0,0 +1,70 @@
+import os, sys, json, random
+from collections import defaultdict as ddict
+
+# upload with something like this:
+#
+# aws s3 cp p2-cr-assignments.json s3://caraza-harter-cs301/a/projects/p2-cr-assignments.json
+
+COURSE = 'b'
+
+# we want submissions that are the latest for a student
+def get_submission_emails(project):
+    # key is user email
+    links = ddict(list)
+    submissions = ddict(list)
+
+    for d, _, file_paths in os.walk('s3/%s/projects/%s' % (COURSE, project)):
+        for path in file_paths:
+            path = os.path.join(d, path)
+            email = path.split(os.sep)[4].replace('*at*', '@')
+            if path.endswith('-link.json'):
+                links[email].append(path)
+            elif path.endswith('submission.json'):
+                submissions[email].append(path)
+
+    for email, values in links.items():
+        latest = sorted(values)[-1]
+        sub = latest.replace('-link.json', os.sep+'submission.json')
+        if sub in submissions[email]:
+            yield email
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 assign-crs.py <PROJECT>")
+        return
+    project = sys.argv[1]
+
+    submitters = [email.replace("*at*", "@") for email in os.listdir(f"s3/{COURSE}/projects/{project}")]
+    if not os.path.exists("roster.json"):
+        print("Need roster.  Run:")
+        print(f"aws s3 cp s3://caraza-harter-cs301/{COURSE}/roster.json roster.json")
+        return
+    with open('roster.json') as f:
+        emails = {row['net_id']+'@wisc.edu' for row in json.load(f) if row['enrolled']}
+    submitters = emails & set(submitters)
+    others = emails - set(submitters)
+    # put people who actually submitted first so we balance better
+    students = sorted(submitters) + sorted(others)
+
+    print(len(submitters), " submitters")
+    print(len(others), " others")
+
+    tas = []
+    with open("tas.json") as f:
+        for row in json.load(f):
+            tas.extend([row['name']] * row['weight'])
+    random.shuffle(tas)
+
+    assignments = {}
+    for i, student in enumerate(students):
+        assignments[student] = tas[i % len(tas)]
+    with open('%s-cr-assignments.json' % project, 'w') as f:
+        json.dump(assignments, f, indent=2)
+
+    print('Run this:')
+    print(f'aws s3 cp %s-cr-assignments.json s3://caraza-harter-cs301/{COURSE}/projects/%s-cr-assignments.json' % (project, project))
+
+
+if __name__ == '__main__':
+     main()
