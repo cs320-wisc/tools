0cba53c1fae3770fe89913b692d14b363e9f289f
Olesia Elfimova <olesiaelfimova@Olesias-Air.resnet.wisc.edu>
2021-11-04 20:24:29 -0500

    fix canvas notifications subject

--- conversation.py
+++ conversation.py
@@ -47,6 +47,7 @@ def send_notification(netid, date, score, project_id):
         "recipients": [str(id)],
         "subject": f"CS 320 - Auto Grader Gave Score {score} to Your {project_id}",
         "body": f"Hello {name}, auto grader gave score {score} to your CS 320 submission made at {date}. Please check your score here: https://tyler.caraza-harter.com/cs320/{conf.SEMESTER}/code_review.html?project_id={project_id}",
+        "force_new": True,
     }
     r = cpost("conversations", payload)
     logging.info(f"Received response {r} from Canvas API")
